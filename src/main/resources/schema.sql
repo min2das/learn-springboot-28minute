a0a36dbde674b8cc47779227fbe6e4375517944c
Mintu Ratan Das <minturatan.das@finconsgroup.com>
2025-03-21 22:22:48 +0100

    pulito per web-app

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
-create table course (
-  id bigint not null primary key,
-  name varchar(255) not null,
-  author varchar(255) not null
-);
+-- create table course (
+--   id bigint not null primary key,
+--   name varchar(255) not null,
+--   author varchar(255) not null
+-- );
